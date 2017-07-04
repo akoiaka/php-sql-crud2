a1651887992744767a93a7c3657919fb7301fb30
akoiaka <akoi@akoi.fr>
2017-07-04 22:26:43 +0200

    en cours .. 2

--- formulaire.php
+++ formulaire.php
@@ -6,40 +6,39 @@
  * Time: 16:30
  */
 
-echo ("<div class='ui inverted segment'>
-<div class='ui inverted form'>
-    <div class='two fields'>
-      <div class='field'>
-        <label>numero de client</label>
-        <input placeholder='client number' type='text'>
-      </div><br>
-      <div class='field'>
-        <label>Nom</label>
-        <input placeholder='Last Name' type='text'>
-      </div>
-      <div class='field'>
-        <label>Prénom</label>
-        <input placeholder='First Name' type='text'>
-      </div>
-      <div class='field'>
-        <label>Date de naissance</label>
-        <input placeholder='Birthday' type='text'>
-      </div>
-      <br/>
-      <div class='field'>
-        <label>Numero de carte de fidélité</label>
-        <input placeholder='Carte de fidélité' type='text'>
-      </div>
-    </div>
-    <div class='inline field'>
-      <div class='ui checkbox'>
-        <input type='checkbox' tabindex='0' class='hidden'>
-        <label>Carte de fidélité</label>
-      </div>
-    </div>
-    <div class='ui submit button'>Submit</div>
-  </div>
-</div>";
+echo ("<div class='ui inverted segment'>"+
+"<div class='ui inverted form'>"+
+    "<div class='two fields'>"+
+      "<div class='field'>"+
+        "<label>numero de client</label>"+
+        "<input placeholder='client number' type='text'>"+
+      "</div>"+
+      "<div class='field'>"+
+        "<label>Nom</label>"+
+        "<input placeholder='Last Name' type='text'>"+
+      "</div>"+
+      "<div class='field'>"+
+        "<label>Prénom</label>"+
+        "<input placeholder='First Name' type='text'>"+
+      "</div>"+
+      "<div class='field'>"+
+        "<label>Date de naissance</label>"+
+        "<input placeholder='Birthday' type='text'>"+
+      "</div>"+
+      "<div class='field'>"+
+        "<label>Numero de carte de fidélité</label>"+
+        "<input placeholder='Carte de fidélité' type='text'>"+
+      "</div>"+
+    "</div>"+
+    "<div class='inline field'>"+
+      "<div class='ui checkbox'>"+
+        "<input type='checkbox' tabindex='0' class='hidden'>"+
+        "<label>Carte de fidélité</label>"+
+      "</div>"+
+    "</div>"+
+    "<div class='ui submit button'>Submit</div>"+
+  "</div>"+
+"/div>");
     ?>
 <!---->
 <!--echo "<div class='ui inverted segment'>-->
